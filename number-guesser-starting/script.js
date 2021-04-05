5e76254d9b24d36e1d181663c822d19f6cf58523
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-04-05 13:35:13 +0100

    first function written

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,8 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// Create a generateTarget() function. 
+// This function will be called at the start of each new round in order to generate the new secret target number.
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+};
